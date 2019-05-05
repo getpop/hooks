bdfded866d3f9e4223f07a0f6bdea7332718adff
Leo <losoviz@gmail.com>
2019-05-05 23:02:12 +0800

    Fixed PHPCS styles

--- tests/contractimplementations/HooksAPI.php
+++ tests/contractimplementations/HooksAPI.php
@@ -6,11 +6,9 @@ class HooksAPI implements \PoP\Hooks\Contracts\HooksAPIInterface
 {
     public function addFilter($tag, $function_to_add, $priority = 10, $accepted_args = 1)
     {
-
     }
     public function removeFilter($tag, $function_to_remove, $priority = 10)
     {
-
     }
     public function applyFilters($tag, $value, ...$args)
     {
@@ -18,14 +16,11 @@ class HooksAPI implements \PoP\Hooks\Contracts\HooksAPIInterface
     }
     public function addAction($tag, $function_to_add, $priority = 10, $accepted_args = 1)
     {
-
     }
     public function removeAction($tag, $function_to_remove, $priority = 10)
     {
-
     }
     public function doAction($tag, ...$args)
     {
-
     }
 }
