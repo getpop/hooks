1baa152f77e08e7d4eab57c1dbcb1a4c26baee76
Leo <losoviz@gmail.com>
2019-05-06 11:54:01 +0800

    Fixed signature

--- src/contracts/HooksAPIInterface.php
+++ src/contracts/HooksAPIInterface.php
@@ -4,9 +4,9 @@ namespace PoP\Hooks\Contracts;
 interface HooksAPIInterface
 {
     public function addFilter(string $tag, $function_to_add, int $priority = 10, int $accepted_args = 1): void;
-    public function removeFilter(string $tag, $function_to_remove, int $priority = 10): void;
+    public function removeFilter(string $tag, $function_to_remove, int $priority = 10): bool;
     public function applyFilters(string $tag, $value, ...$args);
     public function addAction(string $tag, $function_to_add, int $priority = 10, int $accepted_args = 1): void;
-    public function removeAction(string $tag, $function_to_remove, int $priority = 10): void;
+    public function removeAction(string $tag, $function_to_remove, int $priority = 10): bool;
     public function doAction(string $tag, ...$args): void;
 }
--- tests/contractimplementations/HooksAPI.php
+++ tests/contractimplementations/HooksAPI.php
@@ -7,8 +7,9 @@ class HooksAPI implements \PoP\Hooks\Contracts\HooksAPIInterface
     public function addFilter(string $tag, $function_to_add, int $priority = 10, int $accepted_args = 1): void
     {
     }
-    public function removeFilter(string $tag, $function_to_remove, int $priority = 10): void
+    public function removeFilter(string $tag, $function_to_remove, int $priority = 10): bool
     {
+        return true;
     }
     public function applyFilters(string $tag, $value, ...$args)
     {
@@ -17,8 +18,9 @@ class HooksAPI implements \PoP\Hooks\Contracts\HooksAPIInterface
     public function addAction(string $tag, $function_to_add, int $priority = 10, int $accepted_args = 1): void
     {
     }
-    public function removeAction(string $tag, $function_to_remove, int $priority = 10): void
+    public function removeAction(string $tag, $function_to_remove, int $priority = 10): bool
     {
+        return true;
     }
     public function doAction(string $tag, ...$args): void
     {
