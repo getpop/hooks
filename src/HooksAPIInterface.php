1e12f3a859673bf6227a1b3591b5d0d5682904d6
Viktor Szépe <viktor@szepe.net>
2020-09-25 08:00:51 +0200

    Add typehints

--- src/HooksAPIInterface.php
+++ src/HooksAPIInterface.php
@@ -6,10 +6,10 @@ namespace PoP\Hooks;
 
 interface HooksAPIInterface
 {
-    public function addFilter(string $tag, $function_to_add, int $priority = 10, int $accepted_args = 1): void;
-    public function removeFilter(string $tag, $function_to_remove, int $priority = 10): bool;
+    public function addFilter(string $tag, callable $function_to_add, int $priority = 10, int $accepted_args = 1): void;
+    public function removeFilter(string $tag, callable $function_to_remove, int $priority = 10): bool;
     public function applyFilters(string $tag, $value, ...$args);
-    public function addAction(string $tag, $function_to_add, int $priority = 10, int $accepted_args = 1): void;
-    public function removeAction(string $tag, $function_to_remove, int $priority = 10): bool;
+    public function addAction(string $tag, callable $function_to_add, int $priority = 10, int $accepted_args = 1): void;
+    public function removeAction(string $tag, callable $function_to_remove, int $priority = 10): bool;
     public function doAction(string $tag, ...$args): void;
 }
