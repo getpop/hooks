0e0b322ebc8e4b807c9c4ea1478d01d3ad4bc572
leoloso <leo@getpop.org>
2021-10-04 09:13:29 +0000

    Merge pull request #1116 from leoloso/make-autowire-functions-final
    Made all autowire functions final

--- src/AbstractHookSet.php
+++ src/AbstractHookSet.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace PoP\Hooks;
 
-use Symfony\Contracts\Service\Attribute\Required;
-use PoP\Translation\TranslationAPIInterface;
 use PoP\ComponentModel\Instances\InstanceManagerInterface;
 use PoP\Root\Services\AbstractAutomaticallyInstantiatedService;
+use PoP\Translation\TranslationAPIInterface;
+use Symfony\Contracts\Service\Attribute\Required;
 
 abstract class AbstractHookSet extends AbstractAutomaticallyInstantiatedService
 {
@@ -16,7 +16,7 @@ abstract class AbstractHookSet extends AbstractAutomaticallyInstantiatedService
     protected InstanceManagerInterface $instanceManager;
 
     #[Required]
-    public function autowireAbstractHookSet(HooksAPIInterface $hooksAPI, TranslationAPIInterface $translationAPI, InstanceManagerInterface $instanceManager): void
+    final public function autowireAbstractHookSet(HooksAPIInterface $hooksAPI, TranslationAPIInterface $translationAPI, InstanceManagerInterface $instanceManager): void
     {
         $this->hooksAPI = $hooksAPI;
         $this->translationAPI = $translationAPI;
